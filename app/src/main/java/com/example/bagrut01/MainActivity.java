2a705d13fbd720319591fb531e5f463ff3dcf30c
ay4748 <ay4748@bs.amalnet.k12.il>
2024-05-14 22:11:00 +0300

    dodge2

--- app/src/main/java/com/example/bagrut01/MainActivity.java
+++ app/src/main/java/com/example/bagrut01/MainActivity.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int gradeHebrew, gradeSafrut, gradeHistory, gradeEzrahut, gradeBible;
     EditText eTHebrew, eTSafrut, eTHistory, eTEzrahut, eTBible, eTName;
+    String name = " ";
+    int REQUEST_CODE = 369;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,20 +37,49 @@ public class MainActivity extends AppCompatActivity {
 
     public void goToSec(View view)
     {
+
+        if(checkIput())
+        {
+
+            gradeHebrew = Integer.parseInt(eTHebrew.getText().toString());
+            gradeSafrut = Integer.parseInt(eTSafrut.getText().toString());
+            gradeHistory = Integer.parseInt(eTHistory.getText().toString());
+            gradeEzrahut = Integer.parseInt(eTEzrahut.getText().toString());
+            gradeBible = Integer.parseInt(eTBible.getText().toString());
+            name = eTName.getText().toString();
+
+            Intent si = new Intent(this,SecondScreen.class);
+            si.putExtra("Name", name);
+            si.putExtra("Hebrew", gradeHebrew );
+            si.putExtra("Safrut", gradeSafrut );
+            si.putExtra("History", gradeHistory );
+            si.putExtra("Ezrahut", gradeEzrahut );
+            si.putExtra("Bible" ,gradeBible );
+
+            startActivityForResult(si,REQUEST_CODE);
+        }
+
+
+    }
+
+    private boolean checkIput()
+    {
+        if (eTHebrew.getText().toString().isEmpty() || eTSafrut.getText().toString().isEmpty() || eTHistory.getText().toString().isEmpty() || eTEzrahut.getText().toString().isEmpty() || eTBible.getText().toString().isEmpty() || eTName.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, "One or more of the field is empty", Toast.LENGTH_LONG).show();
+            return false;
+        }
         gradeHebrew = Integer.parseInt(eTHebrew.getText().toString());
         gradeSafrut = Integer.parseInt(eTSafrut.getText().toString());
         gradeHistory = Integer.parseInt(eTHistory.getText().toString());
         gradeEzrahut = Integer.parseInt(eTEzrahut.getText().toString());
         gradeBible = Integer.parseInt(eTBible.getText().toString());
-
-        Intent si = new Intent(this,SecondScreen.class);
-        si.putExtra("Hebrew", gradeHebrew );
-        si.putExtra("Safrut", gradeSafrut );
-        si.putExtra("History", gradeHistory );
-        si.putExtra("Ezrahut", gradeEzrahut );
-        si.putExtra("Bible" ,gradeBible );
-
-        startActivity(si);
-
+        name = eTName.getText().toString();
+        if(gradeHebrew < 0 || gradeHebrew > 100 || gradeSafrut < 0 || gradeSafrut > 100 || gradeHistory < 0 || gradeHistory > 100 ||  gradeEzrahut < 0 || gradeEzrahut > 100 || gradeBible < 0 || gradeBible > 100)
+        {
+            Toast.makeText(this, "One or more of the grades are wrong", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/bagrut01/SecondScreen.java
+++ app/src/main/java/com/example/bagrut01/SecondScreen.java
@@ -1,6 +1,11 @@
 package com.example.bagrut01;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,99 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondScreen extends AppCompatActivity {
+    EditText eTUnitsMath,eTGradeMath,eTUnitsEng,eTGradeEng,eTSubjectFirst,eTGradeFirst,eTSubjectSecond,eTSecondGrade,eTSecondHagSub, eTSecHagGrade;
+    RadioButton rB5or0first, rB5or0Second;
+    int gradeHebrew, gradeSafrut, gradeHistory, gradeEzrahut, gradeBible, gradeMath, gradeEnglish, gradeFirst, gradeSecond, gradeThird;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        initViews();
+
+        Intent gi = getIntent();
+        gradeHebrew = gi.getIntExtra("Hebrew", 1);
+        gradeSafrut = gi.getIntExtra("Safrut", 1);
+        gradeHistory = gi.getIntExtra("History", 1);
+        gradeEzrahut = gi.getIntExtra("Ezrahut", 1);
+        gradeBible = gi.getIntExtra("Bible", 1);
+
     }
+
+    private void initViews()
+    {
+        eTUnitsMath = findViewById(R.id.eTUnitsMath);
+        eTGradeMath = findViewById(R.id.eTGradeMath);
+        eTUnitsEng = findViewById(R.id.eTUnitsEng);
+        eTGradeEng = findViewById(R.id.eTGradeEng);
+        eTSubjectFirst = findViewById(R.id.eTSubjectFirst);
+        eTGradeFirst = findViewById(R.id.eTGradeFirst);
+        eTSubjectSecond  = findViewById(R.id.eTSubjectSecond);
+        eTSecondGrade = findViewById(R.id.eTSecondGrade);
+        eTSecondHagSub = findViewById(R.id.eTSecondHagSub);
+        eTSecHagGrade = findViewById(R.id.eTSecHagGrade);
+
+        rB5or0first = findViewById(R.id.rB5or0first);
+        rB5or0Second = findViewById(R.id.rB5or0Second);
+    }
+
+    public void clickedMavoMadaim(View view)
+    {
+        eTSecHagGrade.setVisibility(View.INVISIBLE);
+        rB5or0first.setVisibility(View.INVISIBLE);
+        rB5or0Second.setVisibility(View.INVISIBLE);
+        eTSecondHagSub.setVisibility(View.INVISIBLE);
+    }
+
+    public void clickedhagshni(View view)
+    {
+        eTSecHagGrade.setVisibility(View.VISIBLE);
+        rB5or0first.setVisibility(View.VISIBLE);
+        rB5or0Second.setVisibility(View.VISIBLE);
+        eTSecondHagSub.setVisibility(View.VISIBLE);
+    }
+
+    public void clickedNothird(View view)
+    {
+        eTSecondHagSub.setVisibility(View.INVISIBLE);
+        eTSecHagGrade.setVisibility(View.INVISIBLE);
+
+    }
+
+    public void clikedYesThird(View view) {
+        eTSecondHagSub.setVisibility(View.VISIBLE);
+        eTSecHagGrade.setVisibility(View.VISIBLE);
+    }
+
+    public static boolean containsOnlyLetters(String str) {
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (!Character.isLetter(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void clickedGoThird(View view)
+    {
+
+    }
+
+    private boolean checkInput()
+    {
+        if(eTUnitsMath.getText().toString().isEmpty() || eTGradeMath.getText().toString().isEmpty() || eTUnitsEng.getText().toString().isEmpty() || eTGradeEng.getText().toString().isEmpty() || eTSubjectFirst.getText().toString().isEmpty() || eTSubjectSecond.getText().toString().isEmpty() || eTSecondGrade.getText().toString().isEmpty() || eTSecondHagSub.getText().toString().isEmpty() || eTSecondHagSub.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, "One or more of the field is empty", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        gradeMath = Integer.parseInt(eTGradeMath.getText().toString());
+        gradeEnglish = Integer.parseInt(eTGradeMath.getText().toString());
+        gradeFirst = Integer.parseInt(eTGradeFirst.getText().toString());
+        gradeSecond =
+        if()
+
+    }
+
 }
